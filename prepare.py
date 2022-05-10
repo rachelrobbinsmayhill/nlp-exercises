52ab7f2bd1152700f113ddea867b1ca839fb027f
rachelrobbinsmayhill <rachel.robbinsmayhill@gmail.com>
2022-05-10 11:35:19 -0500

    Initial Commit

---
+++ prepare.py
@@ -0,0 +1,15 @@
+import unicodedata
+import re
+import json
+
+import nltk
+from nltk.tokenize.toktok import ToktokTokenizer
+from nltk.corpus import stopwords
+
+import pandas as pd
+
+import acquire
+
+
+
+
